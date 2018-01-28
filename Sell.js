680ba29edeeb01b38fc5379b308579a016bf8761
copperstick6 <copperstick6@gmail.com>
2018-01-27 22:35:27 -0600

    got nested navigation to work

--- Sell.js
+++ Sell.js
@@ -9,31 +9,16 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
-  Button
+  View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-	'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-	'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class Sell extends Component<{}> {
   render() {
 	return (
 	  <View style={styles.container}>
 		<Text style={styles.welcome}>
-		  Welcome to React Native!
+		 Sell!
 		</Text>
-		<Text style={styles.instructions}>
-		  To get started, edit App.js
-		</Text>
-		<Text style={styles.instructions}>
-		  {instructions}
-		</Text>
-		<Button onPress={() => this.props.navigation.navigate('Transactions')} title="Back"></Button>
 	  </View>
 	);
   }
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import {
-  StackNavigator,
+  TabNavigator,
+  StackNavigator
 } from 'react-navigation';
 
+import Buy from './Buy'
+import Sell from './Sell'
+
+const TransactionsNavigator = TabNavigator({
+	"Buy a Loan": {screen: Buy},
+	"Get a Loan": {screen: Sell}
+})
+
 const Lone = StackNavigator({
-  Home: { screen: App }
+  Home: { screen: App },
+  Transactions: {screen: TransactionsNavigator}
 });
+
+
 
 
 AppRegistry.registerComponent('Lone', () => Lone);
